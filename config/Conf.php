6350a4077e1fd028e8717be0d80be357be371d07
Jawad EL HIDRAOUI <jawad.el-hidraoui@etu.umontpellier.fr>
2022-04-22 15:51:27 +0200

    Model Product created

--- config/Conf.php
+++ config/Conf.php
@@ -1,4 +1,33 @@
 <?php
 
 class Conf{
+    private static $databases = [
+        'hostname' => 'localhost',
+        'database' => 'elhidraouij',
+        'login' => 'elhidraouij',
+        'password' => 'webas'
+    ];
+
+    private static $debug = False;
+
+    public static function getDebug(){
+        return self::$debug;
+    }
+
+    public static function getLogin(){
+        return self::$databases['login'];
+    }
+
+    public static function getHostname(){
+        return self::$databases['hostname'];
+    }
+
+    public static function getDatabase(){
+        return self::$databases['database'];
+    }
+
+    public static function getPassword(){
+        return self::$databases['password'];
+    }
+
 }
