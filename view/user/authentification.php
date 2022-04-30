eea08a54d0cad4d73ea34303c29cd9773dec4b00
Mathis Duban <mathis.duban@etu.umonpellier.fr>
2022-04-30 16:45:20 +0200

    save

--- view/user/authentification.php
+++ view/user/authentification.php
@@ -7,8 +7,8 @@ session_start();
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../src/Styles/Styles.css">
-    <script src="../src/Scripts/Scripts.js"></script>
+    <link rel="stylesheet" href="../../src/Styles/Styles.css">
+    <script src="../../src/Scripts/Scripts.js"></script>
     <title>Authentification</title>
 </head>
 <body id="LoginScreen">
@@ -18,17 +18,17 @@ session_start();
 
     <div id="LeftPart">
 
-        <img src="../src/images/productsPictures/xp_bottle_mc.gif" alt="backgound" id="background_login">
+        <img src="../../src/images/productsPictures/xp_bottle_mc.gif" alt="backgound" id="background_login">
     </div>
 
 
     <div id="RightPart">
-        <form method="post" action="../Controller/connexion.php">
+        <form method="post" action="../../Controller/connexion.php">
         <div id="Title">Member Login</div>
         <div><input type="text" placeholder="Email or login" class="Information" name="mail_login"></div>
-        <div><img id="icon_login" src="../src/images/PageIcons/avatar_icon.png"></div>
+        <div><img id="icon_login" src="../../src/images/PageIcons/avatar_icon.png"></div>
         <div><input type="password" placeholder="Password" name="password" class="Information"></div>
-        <div><img id="icon_password" src="../src/images/PageIcons/email_icon.jpg"></div>
+        <div><img id="icon_password" src="../../src/images/PageIcons/email_icon.jpg"></div>
         <input  type="submit" value="Log in" id="buttonLogin"/>
         <div id="forgottenID"> forgotten Username / Password ?</div>
         <div id="createAccount">Create your account -></div>
